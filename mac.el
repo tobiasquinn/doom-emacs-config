0e7ade160b96cba8b8844e1997f92461a900d09a
Tobias Quinn <tobias@tobiasquinn.com>
2020-12-28 19:04:08 +0000

    frame, home, end and selection keys for mac

--- mac.el
+++ mac.el
@@ -14,3 +14,29 @@
         "<s-down>" #'cider-repl-forward-input
         "<s-right>" #'right-word
         "<s-left>" #'left-word))
+
+(when-let (dims (doom-store-get 'last-frame-size))
+  (cl-destructuring-bind ((left . top) width height fullscreen) dims
+    (setq initial-frame-alist
+          (append initial-frame-alist
+                  `((left . ,left)
+                    (top . ,top)
+                    (width . ,width)
+                    (height . ,height)
+                    (fullscreen . ,fullscreen))))))
+
+(defun save-frame-dimensions ()
+  (doom-store-put 'last-frame-size
+                  (list (frame-position)
+                        (frame-width)
+                        (frame-height)
+                        (frame-parameter nil 'fullscreen))))
+
+(add-hook 'kill-emacs-hook #'save-frame-dimensions)
+
+(map! "s-v" #'clipboard-yank)
+(map! "<home>" #'beginning-of-line)
+(map! "<end>" #'end-of-line)
+
+(setq select-enable-primary t)
+(setq select-enable-clipboard nil)
